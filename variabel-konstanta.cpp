f3ecc63a0aaaf66ea03512524a4f5c3c6538e40f
aeryb <89185530+aeryb@users.noreply.github.com>
2021-08-23 10:42:31 +0800

    Create variabel-konstanta.cpp

---
+++ variabel-konstanta.cpp
@@ -0,0 +1,19 @@
+/*Mengenal */
+#include <iostream>
+#define sapaan1 "\tSelamat Datang di Mata Kuliah Struktur Data\n";
+#define sapaan2 "\tAnda akan berkenalan dengan Bahasa Pemprograman C++\n";
+#define nama "......................."; //Isilah titik-titik dengan nama lengkap Anda
+#define nim "......................."; //Isilah titik-titik dengan NIM Anda
+
+using namespace std;
+
+int main () {
+  cout << sapaan1;
+  cout << sapaan2;
+  cout << "\n\tNama Anda adalah\t: ";
+  cout << nama;
+  cout << "\n\tNIM Anda adalah\t\t: ";
+  cout << nim;
+  cout << "\n\n\tSELAMAT BELAJAR BAHASA PEMPROGRAMAN C++";
+  return 0;
+}
